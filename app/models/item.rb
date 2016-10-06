34a767df310b5e43eb7bea347a85968f1747cb81
Bernard Alvero <bernard.alvero@gmail.com>
2016-10-06 15:03:28 +0800

    Added User Item association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 120 }
   validates :description, presence: true, length: { minimum: 3 }
 end
