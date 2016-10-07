45bd62a642324a80c7d713d884713bec31b8e810
Bernard Alvero <bernard.alvero@gmail.com>
2016-10-07 13:49:52 +0800

    Add Completed_at

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 120 }
   validates :description, presence: true, length: { minimum: 3 }
+
+  def completed?
+    !completed_at.blank?
+  end
+
 end
